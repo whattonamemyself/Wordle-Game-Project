23ac6333a8c3df483de9462e9fd70456475607d3
reknahS <shankernram@gmail.com>
2022-02-07 14:37:23 -0800

    t

--- wordleword.py
+++ wordleword.py
@@ -10,10 +10,41 @@
 #    setMisplaced(pos) - integer - set character are pos misplaced and colors accordingly
 #    setNotUsed(pos) - integer - set character are pos misplaced and colors accordingly
 #===========================================================================
+#===========================================================================
+# class FancyWord
+# Description: a colored word - each letter has a color attribute
+#
+# State Attributes
+#   - word - String - the word
+#   - color - List - a list of strings which indicate the color or each word
+#                  - colors: 'normal', 'green', 'yellow', 'red', 'blue' and 'gray' are supported
+#
+# Methods
+#   - getWord() - returns the word
+#   - setColor(color) - sets the color of the entire word to 'color'
+#   - charAt(pos) - returns the letter of the word at pos
+#   - getChColor(pos) - returns the color of the letter at pos
+#   - setChColor(pos, color) - sets the color of the letter at pos to color
+#   - __str__() - returns an ANSI colored string of the word
+#   - __eq__() - compares the word of two Fancy words
+#===========================================================================
 from fancyword import FancyWord
 
 # TODO - make WordleWord
 
 class WordleWord(FancyWord):
-    def __init__():
-        x = "uwu"
+    def __init__(self, w):
+        super().__init__(w)
+    def isCorrect(self, pos):
+        return self.color[pos] == "green"
+    def isMisplaced(self, pos):
+        return self.color[pos] == "yellow"
+    def isNotUsed(self, pos):
+        return self.color[pos] == "gray"
+    def setCorrect(self, pos):
+        self.color[pos] = "green"
+    def setMisplaced(self, pos):
+        self.color[pos] = "yellow"
+    def setNotUsed(self, pos):
+        self.color[pos] = "gray"
+    
