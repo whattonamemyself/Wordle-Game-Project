a14024b71f9e6496e54575c42d2763235d6c27ac
reknahS <shankernram@gmail.com>
2022-02-09 15:24:28 -0800

    gui start

--- GUITEST.py
+++ GUITEST.py
@@ -1,7 +1,13 @@
-
 import tkinter as tk
 class Screen:
     def __init__(self):
         self.window = tk.Tk()
-        self.heading = tk.Label(text = "WORDLE")
+        self.window.geometry("2000x1000")
+        self.tab = tk.Button(self.window, height = 1, width = 250, 
+                             text = "Wordle+", bg  = "green", font = "SF", 
+                             command = self.hello)
+        self.tab.pack()
+        self.window.mainloop()
+    def hello(self):
+        print("hello")
 screen = Screen()
