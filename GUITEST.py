02d41b28450fa052d3e41fdaa1efd79b673c76bf
reknahS <shankernram@gmail.com>
2022-02-10 14:34:14 -0800

    g

--- GUITEST.py
+++ GUITEST.py
@@ -3,9 +3,12 @@ class Screen:
     def __init__(self):
         self.window = tk.Tk()
         self.window.geometry("2000x1000")
-        self.window.configure(background="white")
-        self.tab = tk.Label(self.window, font = "Rubik", height = 2, 
-                            width = 3, text = "Wordle+", fg = "black")
-        self.tab.pack()
+        self.window.configure(background="black")
+        self.heading = tk.Label(self.window, font = "Rubik", height = 2, 
+                            width = 8, text = "Wordle+", bg = "black", fg = "white")
+        self.heading.place(relx = 0.5,
+                   rely = 0.5,
+                   anchor = 'center')
+        self.heading.pack()
         self.window.mainloop()
 screen = Screen()
