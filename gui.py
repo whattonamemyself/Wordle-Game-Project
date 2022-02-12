7f67768a73e58f3245abee2ca7b90ca720487a39
reknahS <shankernram@gmail.com>
2022-02-11 20:29:39 -0800

    k

--- gui.py
+++ gui.py
@@ -3,23 +3,27 @@ from tkinter import *
 class Screen:
     def __init__(self):
         self.window = Tk()
-        self.canvas = Canvas(width = 2000, height = 1000, bg = "black")
+        self.canvas = Canvas(width = 1000, height = 600, bg = "black")
         self.canvas.pack()
-        self.heading = Label(self.window, text = "Wordle +", font = "Rubik 40 bold", fg = "white", bg = "black")
-        self.heading.place(x = 960, y = 30, anchor = CENTER)
+        self.heading = Label(self.window, text = "WORDLE +", font = "Rubik 40 bold", fg = "white", bg = "black")
+        self.heading.place(x = 500, y = 30, anchor = CENTER)
         self.squares = []
-        for x in range(770, 1170, 80):
+        for x in range(380, 630, 50):
             square = []
-            for y in range(175, 605, 80):
-                square.append([x,y,x+70,y-70])
+            for y in range(115, 415, 50):
+                square.append([x,y,x+40,y-40])
             self.squares.append(square)
         for i in self.squares:
             for j in i:
                 self.canvas.create_rectangle(j[0], j[1], j[2], j[3], outline = "white")
-        #self.alphabet = []
-        # alphabet = "abcdefghijklmnopqrstuvwxyz".upper()
-        # for x in range(670, 1270, 60):
-        #     self.letter = Label(self.window, text = alphabet[])
+        test = Button(self.window, text = "PRESS ME", fg = "white", bg = "black", command = self.window.quit)
+        test.pack()
+        self.window.mainloop()
+    def squareCorrect(self, guess, pos):
+        coords = self.squares[guess][pos]
+        self.canvas.create_rectangle(coords[0], coords[1], coords[2], coords[3], fill = "green", outline = "white")
         self.window.mainloop()
 screen = Screen()
+
+screen.squareCorrect(3, 2)
 
