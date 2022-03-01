5765c539064ab07b9504e245b3d526f3cbeb5d15
whattonamemyself <erichsu467@gmail.com>
2022-02-28 16:28:23 -0800

    w

--- confettiuwu.py
+++ confettiuwu.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random
 import math
+from inputwrapper import InputWrapper
 
 def renderShape(canvas, canvasitems, numpoints, size, rot, xpos, ypos, ghost, col):
     points = []
@@ -16,8 +17,8 @@ def renderShape(canvas, canvasitems, numpoints, size, rot, xpos, ypos, ghost, co
     
     gh = min(ghost, 255)/255
     r = round(col[0]*(gh) + 255 * (1-gh))
-    g = round(col[0]*(gh) + 255 * (1-gh))
-    b = round(col[0]*(gh) + 255 * (1-gh))
+    g = round(col[1]*(gh) + 255 * (1-gh))
+    b = round(col[2]*(gh) + 255 * (1-gh))
     col = hex(r*256*256 + g*256 + b)[2:]
     while len(col) < 6:
         col = '0' + col
@@ -25,7 +26,8 @@ def renderShape(canvas, canvasitems, numpoints, size, rot, xpos, ypos, ghost, co
     shape = canvas.create_polygon(points,outline = "", fill = col, width = 0)
     canvasitems.append(shape)
 class Confetti():
-    def __init__(self,canvas, window):
+    def __init__(self,canvas, window, inputs):
+        self.inputs = inputs
         self.canvas = canvas
         self.window = window
         self.canvasitems = []
@@ -40,8 +42,8 @@ class Confetti():
         self.delay = []
         self.ghost = []
         self.col = []
-        for i in range(200):
-            self.delay.append(random.randint(1,50))
+        for i in range(150):
+            self.delay.append(random.randint(1,40))
             self.ghost.append(400)
             force = random.random() * 1.25
             self.shape.append(random.randint(0,2))
@@ -49,7 +51,8 @@ class Confetti():
             self.rot.append(random.randint(0,360))
             self.yv.append(random.random()*-20 + force * -5 - 10)
             self.ypos.append(600)
-            self.col.append((random.randint(0,1)*255, random.randint(0,1)*255, random.randint(0,1)*255))
+            mask = random.randint(1,6) # a simple bitmask of the colors 
+            self.col.append(((mask%2)*255, (mask//2%2)*255, (mask//4%2)*255))
             if random.randint(0,1) == 0:
                 self.xpos.append(0)
                 self.xv.append(random.random()*15 + force * 10)
@@ -58,26 +61,28 @@ class Confetti():
                 self.xv.append(random.random()*-15 + force * -10)
 
     def update(self): #updates every frame
-        for i in self.canvasitems:
-            self.canvas.delete(i)
-        if self.tick > 20:
-            for i in range(200):
-                if self.delay[i] < self.tick :
-                    self.xpos[i] += self.xv[i]
-                    self.ypos[i] += self.yv[i]
-                    self.yv[i] += 1
-                    self.xv[i] *= 0.95
-                    self.ghost[i] -= 10
-                    if self.ghost[i] > 0:
-                        renderShape(self.canvas, self.canvasitems, self.shape[i] + 3, self.size[i], self.rot[i], self.xpos[i], self.ypos[i], self.ghost[i], self.col[i])
-        self.tick += 1
+        if not self.inputs.isMouseDown():
+            for i in self.canvasitems:
+                self.canvas.delete(i)
+            if self.tick > 20:
+                for i in range(150):
+                    if self.delay[i] < self.tick :
+                        self.xpos[i] += self.xv[i]
+                        self.ypos[i] += self.yv[i]
+                        self.yv[i] += 1 #gravity
+                        self.xv[i] *= 0.95 #deceleration
+                        self.ghost[i] -= 10
+                        if self.ghost[i] > 0 and self.ypos[i] < 620:
+                            renderShape(self.canvas, self.canvasitems, self.shape[i] + 3, self.size[i], self.rot[i], self.xpos[i], self.ypos[i], self.ghost[i], self.col[i])
+            self.tick += 1
         self.window.after(16, self.update)
 
 def main():
     window = tk.Tk()
     canvas = tk.Canvas(width = 1000, height = 600)
     canvas.pack()
-    confetti = Confetti(canvas, window)
+    inputs = InputWrapper(canvas)
+    confetti = Confetti(canvas, window, inputs)
     confetti.update()
     window.mainloop()
 if __name__ == "__main__":
