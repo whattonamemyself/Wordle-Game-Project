94c3c15e5535d6c79363aaea6247ac9509b9d170
whattonamemyself <erichsu467@gmail.com>
2022-03-03 14:11:19 -0800

    d

--- actualgame.py
+++ actualgame.py
@@ -1,12 +1,21 @@
 from wordbank import WordBank
 from gui import Screen
 from tkinter import *
+from wordsearchgui import WSGUI
+from wordsearch import WordSearch
+from inputwrapper import InputWrapper
 
+window = Tk()
 def actualgame():
-    window = Tk()
     canvas = Canvas(window, width = 1000, height = 600, bg = "black")
     all_words = WordBank("words_alpha.txt")
     word = all_words.getRandom()
+    ws = WordSearch(20,20)
+    ws.genWordSearch()
+    inputs = InputWrapper(canvas)
+    gui = WSGUI(ws,inputs, canvas, window)
+    gui.update()
     screen = Screen(canvas, window, False, word, 6)
     print(screen.getGuess())
+    window.mainloop()   
 actualgame()
